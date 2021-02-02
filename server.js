b8287106b52e713a584a90098fb01a0681f08f54
desertsniper87 <torshobuet@gmail.com>
2021-02-02 16:05:27 +0600

    commit for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const port = 80
+const port = process.env.PORT || 3000
 
 const jhuEdu = require('./api/jhu-edu')
 const kcdc = require('./api/korea-kcdc')
@@ -13,4 +13,8 @@ app.use('/kcdc', kcdc)
 
 app.get('/', (req, res) => res.send('API Service for tracking the COVID-19!'))
 
-app.listen(port, () => console.log(`API Server listening on port ${port}!`))
+app.listen(port, err => {
+    if(err) throw err;
+    console.log("%c Server running", "color: green");
+    console.log(`API Server listening on port ${port}!`)
+});
